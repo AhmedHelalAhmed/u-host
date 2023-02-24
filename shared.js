adecf6c76b15df3353bde8f20e8d0e8647cd5b2a
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-02-24 05:17:17 +0200

    ♻️refactor to classes ♻️

--- shared.js
+++ shared.js
@@ -7,25 +7,24 @@ const closeModelButton = document.querySelector('.modal__action--negative');
 const toggleButton = document.querySelector('.toggle-button');
 const mobileNav = document.querySelector('.mobile-nav');
 const closeModel = () => {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+    backdrop.classList.remove('open');
+    modal.classList.remove('open');
 };
 
 for (const selectPlanButton of selectPlanButtons) {
     selectPlanButton.addEventListener("click", () => {
-        backdrop.style.display = 'block';
-        modal.style.display = 'block';
+        backdrop.classList.add('open');
+        modal.classList.add('open');
     });
 }
 
 closeModelButton.addEventListener("click", closeModel);
 backdrop.addEventListener("click", () => {
-    mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
     closeModel();
 });
 toggleButton.addEventListener("click", () => {
-    mobileNav.style.display = 'block';
-    mobileNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 });
 
