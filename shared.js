9b2f4bc080a58a47a1ee271ee4765e4f8950c029
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-02-24 05:08:34 +0200

    ✨ implement hamburger menu actions ✨

--- shared.js
+++ shared.js
@@ -3,18 +3,29 @@ const modal = document.querySelector('.modal');
 
 const selectPlanButtons = document.querySelectorAll('.plan button');
 const closeModelButton = document.querySelector('.modal__action--negative');
-const closeModalOptions = [backdrop, closeModelButton];
+
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
+const closeModel = () => {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+};
 
 for (const selectPlanButton of selectPlanButtons) {
-    selectPlanButton.addEventListener("click", (e) => {
+    selectPlanButton.addEventListener("click", () => {
         backdrop.style.display = 'block';
         modal.style.display = 'block';
     });
 }
 
-for (const closeModalOption of closeModalOptions) {
-    closeModalOption.addEventListener("click", (e) => {
-        backdrop.style.display = 'none';
-        modal.style.display = 'none';
-    });
-}
+closeModelButton.addEventListener("click", closeModel);
+backdrop.addEventListener("click", () => {
+    mobileNav.style.display = 'none';
+    closeModel();
+});
+toggleButton.addEventListener("click", () => {
+    mobileNav.style.display = 'block';
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
+
